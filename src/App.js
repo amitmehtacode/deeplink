0c47f534a9498d350810b55f6c586c4a1e009170
Amit Kumar <amit.kumar8@tothenew.com>
2024-08-28 11:20:29 +0530

    minor fix

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
 
 function App() {
+
+  useEffect(() => {
+    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+    console.log('userAgent>>', userAgent)
+
+    if (/android/i.test(userAgent)) {
+      window.location.href = "https://idanim.sng.link/Bl2li/rvq9/l0s9";
+    } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+      window.location.href = "https://idanim.sng.link/Cl2li/htfa/69jl";
+    } else {
+      window.location.href = 'https://www.idanim.com/idanim-mobile-app-download'
+    }
+  }, []);
+
+  
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Let's start Deep Linking :) :p</p>
       </header>
     </div>
   );
